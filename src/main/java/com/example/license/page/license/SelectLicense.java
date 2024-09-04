31d4bbd6b6868087e0d868e524cb3508b7294f6b
yamahira0126 <b2222490@photon.chitose.ac.jp>
2024-09-05 00:29:37 +0900

    SelectLicense完成

--- src/main/java/com/example/license/page/license/SelectLicense.java
+++ src/main/java/com/example/license/page/license/SelectLicense.java
@@ -2,9 +2,9 @@ package com.example.license.page.license;
 
 
 import com.example.license.MySession;
-import com.example.license.data.License;
+import com.example.license.data.*;
 import com.example.license.page.common.MainMenu;
-import com.example.license.service.ILicenseService;
+import com.example.license.service.*;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.link.Link;
@@ -15,16 +15,29 @@ import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.wicketstuff.annotation.mount.MountPath;
 
 import java.text.SimpleDateFormat;
+import java.util.Optional;
 
 @MountPath("SelectLicense")
 public class SelectLicense extends MainMenu {
 
     @SpringBean
     private ILicenseService licenseService;
+    @SpringBean
+    private ISoftwareService softwareService;
+    @SpringBean
+    private IBudgetService budgetService;
+    @SpringBean
+    private ITerminalService terminalService;
+    @SpringBean
+    private IAccountService accountService;
 
      public SelectLicense() {
 
          var licensesModel = Model.ofList(licenseService.findLicenses(MySession.get().getAccount()));
+         var softwareList = softwareService.findSoftwares(MySession.get().getAccount());
+         var budgetList = budgetService.findBudgets(MySession.get().getAccount());
+         var terminalList = terminalService.findTerminals(MySession.get().getAccount());
+         var accountList = accountService.findAccounts(MySession.get().getAccount());
 
          var licensesLV = new ListView<>("licenses", licensesModel) {
              @Override
@@ -32,15 +45,31 @@ public class SelectLicense extends MainMenu {
                  var itemModel = listItem.getModel();
                  var license = itemModel.getObject();
 
+                 Optional<Software> softwareOpt = softwareList.stream()
+                         .filter(software -> software.getSoftwareId().equals(license.getSoftwareId())).findFirst();
+                 var softwareName = softwareOpt.orElse(null).getSoftwareName();
+
+                 Optional<Budget> budgetOpt = budgetList.stream()
+                         .filter(budget -> budget.getBudgetId().equals(license.getBudgetId())).findFirst();
+                 var budgetName = budgetOpt.orElse(null).getBudgetName();
+
+                 Optional<Terminal> terminalOpt = terminalList.stream()
+                         .filter(terminal -> terminal.getTerminalId().equals(license.getTerminalId())).findFirst();
+                 var terminalName = terminalOpt.orElse(null).getTerminalName();
+
+                 Optional<Account> accountOpt = accountList.stream()
+                         .filter(account -> account.getAccountId().equals(license.getAccountId())).findFirst();
+                 var accountName = accountOpt.orElse(null).getAccountName();
+
                  SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
 
                  var licenseIdModel = Model.of(license.getLicenseId());
                  var licenseIdLabel = new Label("licenseId", licenseIdModel);
                  listItem.add(licenseIdLabel);
 
-                 var softwareIdModel = Model.of(license.getSoftwareId());
-                 var softwareIdLabel = new Label("softwareId", softwareIdModel);
-                 listItem.add(softwareIdLabel);
+                 var softwareNameModel = Model.of(softwareName);
+                 var softwareNameLabel = new Label("softwareName", softwareNameModel);
+                 listItem.add(softwareNameLabel);
 
                  var licenseStartDateModel = Model.of(formatter.format(license.getLicenseStartDate()));
                  var licenseStartDateLabel = new Label("licenseStartDate", licenseStartDateModel);
@@ -50,17 +79,17 @@ public class SelectLicense extends MainMenu {
                  var licenseEndDateLabel = new Label("licenseEndDate", licenseEndDateModel);
                  listItem.add(licenseEndDateLabel);
 
-                 var budgetIdModel = Model.of(license.getBudgetId());
-                 var budgetIdLabel = new Label("budgetId", budgetIdModel);
-                 listItem.add(budgetIdLabel);
+                 var budgetNameModel = Model.of(budgetName);
+                 var budgetNameLabel = new Label("budgetName", budgetNameModel);
+                 listItem.add(budgetNameLabel);
 
-                 var terminalIdModel = Model.of(license.getTerminalId());
-                 var terminalIdLabel = new Label("terminalId", terminalIdModel);
-                 listItem.add(terminalIdLabel);
+                 var terminalNameModel = Model.of(terminalName);
+                 var terminalNameLabel = new Label("terminalName", terminalNameModel);
+                 listItem.add(terminalNameLabel);
 
-                 var accountIdModel = Model.of(license.getAccountId());
-                 var accountIdLabel = new Label("accountId", accountIdModel);
-                 listItem.add(accountIdLabel);
+                 var accountNameModel = Model.of(accountName);
+                 var accountNameLabel = new Label("accountName", accountNameModel);
+                 listItem.add(accountNameLabel);
 
                  var serialCodeModel = Model.of(license.getSerialCode());
                  var serialCodeLabel = new Label("serialCode", serialCodeModel);
