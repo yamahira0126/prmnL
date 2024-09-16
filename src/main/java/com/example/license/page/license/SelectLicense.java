c7f3493fe219bcb4a222d096aa62e7eda9bc389f
chocoaisu <b2220970@photon.chitose.ac.jp>
2024-09-16 23:41:57 +0900

    paging commit

--- src/main/java/com/example/license/page/license/SelectLicense.java
+++ src/main/java/com/example/license/page/license/SelectLicense.java
@@ -3,14 +3,18 @@ package com.example.license.page.license;
 
 import com.example.license.MySession;
 import com.example.license.data.*;
+import com.example.license.page.budget.SelectBudget;
 import com.example.license.page.common.MainMenu;
 import com.example.license.service.*;
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.list.ListItem;
 import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.request.resource.JavaScriptResourceReference;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.wicketstuff.annotation.mount.MountPath;
 
@@ -122,4 +126,11 @@ public class SelectLicense extends MainMenu {
          var toMakeBudget = new BookmarkablePageLink<>("toMakeLicense", MakeLicense.class);
          add(toMakeBudget);
      }
+
+    @Override
+    public void renderHead(IHeaderResponse response) {
+        super.renderHead(response);
+        response.render(JavaScriptHeaderItem.forReference(new JavaScriptResourceReference(SelectBudget.class, "../js/sort.js")));
+        response.render(JavaScriptHeaderItem.forReference(new JavaScriptResourceReference(SelectBudget.class, "../js/paging.js")));
+    }
 }
